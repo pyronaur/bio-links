3821c183bc90b0fdd217cd1107fc686b9d8826c5
Norris <im@justnorris.com>
2017-12-28 09:55:36 +0200

    Meta class instantiation helpers

--- public/functions/functions.php
+++ public/functions/functions.php
@@ -9,11 +9,30 @@ function biolinks_instance() {
 	return Bio_Links_Plugin::instance();
 }
 
+global $biolinks_current_meta;
+function biolinks_current_meta() {
+
+	global $biolinks_current_meta;
+	if ( ! isset( $biolinks_current_meta ) ) {
+		$biolinks_current_meta = biolinks_new_meta_instance();
+	}
+
+	return $biolinks_current_meta;
+}
+
+function biolinks_new_meta_instance() {
+
+	return new Bio_Links_Plugin\Frontend\Biolinks_Meta( get_the_ID() );
+}
+
+
+
 /**
  * Detect whether current layout is an archive layout
  *
  * @return bool
  */
 function biolinks_is_queried() {
+
 	return ( 'biolink' === get_post_type() );
 }
