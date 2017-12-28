96ba6beb4a11f2580510e9ff062c706fbcb53c66
Norris <im@justnorris.com>
2017-12-28 12:48:47 +0200

    Add hover states

--- public/templates/default/header.php
+++ public/templates/default/header.php
@@ -4,9 +4,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>"
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php do_action( 'biolinks/header' ) ?>
-
 	<?php /** @TODO: Move this to the `biolinks/header` action and utilize WP_Styles and WP_Dependencies classes */ ?>
 	<link rel="stylesheet" href="<?php echo BIOLINKS_PLUGIN_DIR_URL ?>public/build/biolinks.css"/>
+	<?php do_action( 'biolinks/header' ) ?>
 </head>
 <body class="biolinks-page <?php biolinks_body_class() ?>">
